1a21845104cb37e0dd009c6a1013bff299c3da5b
hackcode <hackcode@gmail.com>
2017-06-15 21:20:25 +0800

    update plugins

--- adblock.py
+++ adblock.py
@@ -1,8 +1,36 @@
 ﻿# -*- coding: utf-8 -*-
 import pymysql  
 import datetime
+
+# 如果不希望加载本插件，可以在配置文件中的 plugins 选项中删除 qqbot.plugins.chatlog 
+from qqbot.utf8logger import DEBUG
+
+def onInit(bot):
+    # 初始化时被调用
+    # 注意 : 此时 bot 尚未启动，因此请勿在本函数中调用 bot.List/SendTo/GroupXXX/Stop/Restart 等接口
+    #       只可以访问配置信息 bot.conf
+    # bot : QQBot 对象
+    DEBUG('ON-INIT : qqbot.plugins.sampleslots')
+
+def onQrcode(bot, pngPath, pngContent):
+    # 获取到二维码时被调用
+    # 注意 : 此时 bot 尚未启动，因此请勿在本函数中调用 bot.List/SendTo/GroupXXX/Stop/Restart 等接口
+    #       只可以访问配置信息 bot.conf
+    # bot : QQBot 对象
+    # pngPath : 二维码图片路径
+    # pngContent : 二维码图片内容
+    DEBUG('ON-QRCODE: %s (%d bytes)', pngPath, len(pngContent))
+
 def onQQMessage(bot, contact, member, content):
+    # 当收到 QQ 消息时被调用
+    # bot     : QQBot 对象，提供 List/SendTo/GroupXXX/Stop/Restart 等接口，详见文档第五节
+    # contact : QContact 对象，消息的发送者
+    # member  : QContact 对象，仅当本消息为 群或讨论组 消息时有效，代表实际发消息的成员
+    # content : str 对象，消息内容
 	if contact.ctype == 'group':
+		if  '麻将'  in content:
+			kickUser(bot, contact, member, content)
+			insertChatAdContent(bot,contact,member,content,'麻将')
 		if  '蓝鲸'  in content:
 			kickUser(bot, contact, member, content)
 			insertChatAdContent(bot,contact,member,content,'蓝鲸')
@@ -92,6 +120,42 @@ def onQQMessage(bot, contact, member, content):
 		if  '欢迎小窗口联系' in content:
 			shutUser(bot, contact, member, content)			
 			insertChatAdContent(bot,contact,member,content,'欢迎小窗口联系')
+			
+
+def onInterval(bot):
+    # 每隔 5 分钟被调用
+    # bot : QQBot 对象，提供 List/SendTo/GroupXXX/Stop/Restart 等接口，详见文档第五节
+    DEBUG('INTERVAL')
+
+def onStartupComplete(bot):
+    # 启动完成时被调用
+    # bot : QQBot 对象，提供 List/SendTo/GroupXXX/Stop/Restart 等接口，详见文档第五节
+    DEBUG('START-UP-COMPLETE')
+
+def onUpdate(bot, tinfo):
+    # 某个联系人列表更新时被调用
+    # bot : QQBot 对象，提供 List/SendTo/GroupXXX/Stop/Restart 等接口，详见文档第五节
+    # tinfo : 联系人列表的代号，详见文档中关于 bot.List 的第一个参数的含义解释
+    DEBUG('ON-UPDATE: %s', tinfo)
+
+def onPlug(bot):
+    # 本插件被加载时被调用，提供 List/SendTo/GroupXXX/Stop/Restart 等接口，详见文档第五节
+    # 提醒：如果本插件设置为启动时自动加载，则本函数将延迟到登录完成后被调用
+    # bot ： QQBot 对象
+    DEBUG('ON-PLUG : qqbot.plugins.sampleslots')
+
+def onUnplug(bot):
+    # 本插件被卸载时被调用
+    # bot ： QQBot 对象，提供 List/SendTo/GroupXXX/Stop/Restart 等接口，详见文档第五节
+    DEBUG('ON-UNPLUG : qqbot.plugins.sampleslots')
+
+def onExpire(bot):
+    # 登录过期时被调用
+    # 注意 : 此时登录已过期，因此请勿在本函数中调用 bot.List/SendTo/GroupXXX/Stop/Restart 等接口
+    #       只可以访问配置信息 bot.conf
+    # bot : QQBot 对象
+    DEBUG('ON-EXPIRE')
+
 
 #踢出用户出群		
 def kickUser(bot, contact, member, content):
@@ -120,8 +184,8 @@ def insertChatAdContent(bot,contact,member,content,keyword):
 		host='localhost',  
 		port=3306,  
 		user='root',  
-		passwd='mysql',  
-		db='it_work_db',  
+		passwd='root',  
+		db='qq_data',  
 		charset='utf8'  
 	)  
 	  
